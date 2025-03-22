5fce5b9f06e64e1e9b22657f42481f81876cb8dd
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-03-22 13:11:11 -0500

    2D Arrays

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+let allScores = [
+    "Jen"[80, 90, 100],
+    "Tom"[70, 82, 93],
+    "Sam"[90, 75, 100],
+];
+// // let studentOne = allScores[0];
+// // let studentTwo = allScores[1];
+// // let studentThree = allScores [3];
+// console.log(allScores);
+
+let theaterSeating = [
+    "Row 1"["1A","1B","1C","1D"],
+    "Row 2"["2D","2E","2F","2G"],
+    "Row 3"["3G","3H","3I","3J"],
+];
+console.log(theaterSeating);
+
+let contactlist = [
+    ["Name", "Email", "Phone"],
+    ["Tile", "Address", "Company Name"],
+];
+
